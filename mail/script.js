20d5dd42196ca5865a84bc0988b8fd2188329dc6
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-27 19:21:12 +0100

    feat: lista creata

---
+++ mail/script.js
@@ -0,0 +1,4 @@
+// Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+// Crea una lista di email di invitati ad una festa.
+const inviti = ["erika@example.it", "andrea@example.it", "francesca@example.it", "luca@example.it",];
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
